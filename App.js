6345083ee11e7c5887f2fa97b590c9cacd5065f3
iceofice <renireas@gmail.com>
2021-08-16 22:14:08 +0700

    add eslint and prettier config

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Image source={require("./assets/icon.png")} style={styles.image} />
+			<Text style={styles.text1}>Beal</Text>
+			<Text style={styles.text}>Home For Barterers.</Text>
+			<Text style={styles.text}>Let’s begin your barter journey!</Text>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#031956",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	image: {
+		width: 100,
+		height: 100,
+		resizeMode: "contain",
+	},
+	text: {
+		color: "#BBC2D8",
+	},
 });
