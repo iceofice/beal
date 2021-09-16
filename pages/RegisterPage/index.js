556b687e060ace2f6724548fb1b4738d2eeeec7c
iceofice <renireas@gmail.com>
2021-09-16 22:23:53 +0700

    Add dismiss keyboard

--- pages/RegisterPage/index.js
+++ pages/RegisterPage/index.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { Image, View, StyleSheet, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
-
+import {
+	Image,
+	View,
+	StyleSheet,
+	KeyboardAvoidingView,
+	Platform,
+	SafeAreaView,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
 import colors from "../../assets/colors/colors";
 import { Button, Text, InputText } from "../../components";
 
@@ -10,64 +18,68 @@ const RegisterPage = () => {
 			style={{ flex: 1 }}
 			behavior={Platform.OS === "ios" ? "padding" : "height"}
 		>
-			<ScrollView style={{ flex: 1 }} contentContainerStyle={styles.container}>
-				<View style={styles.iconWrapper}>
-					<Image source={require("../../assets/images/icon.png")} style={styles.icon} />
-				</View>
+			<SafeAreaView style={{ flex: 1 }}>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View style={styles.container}>
+						<View style={styles.iconWrapper}>
+							<Image source={require("../../assets/images/icon.png")} style={styles.icon} />
+						</View>
 
-				<View style={styles.formWrapper}>
-					<Text weight="bold" size={36}>
-						Register
-					</Text>
-					<View style={{ flex: 1.5, justifyContent: "space-between", marginVertical: 40 }}>
-						<InputText />
-						<InputText />
-						<InputText />
-					</View>
-					<View style={{ flex: 2, alignItems: "center" }}>
-						<Button style={{ marginVertical: 12 }} title="Register" />
-						<View style={{ flexDirection: "row" }}>
-							<Text weight="light" size={14} color={colors.textLight}>
-								Already have an account?
+						<View style={styles.formWrapper}>
+							<Text weight="bold" size={36}>
+								Register
 							</Text>
-							<Text
-								weight="bold"
-								size={14}
-								color={colors.text}
-								style={{
-									marginHorizontal: 4,
-									borderBottomColor: colors.text,
-									borderBottomWidth: 1,
-								}}
-							>
-								Login
-							</Text>
-							<Text weight="light" size={14} color={colors.textLight}>
-								here!
-							</Text>
+							<View style={{ flex: 1.5, justifyContent: "space-between", marginVertical: 40 }}>
+								<InputText />
+								<InputText />
+								<InputText />
+							</View>
+							<View style={{ flex: 2, alignItems: "center" }}>
+								<Button style={{ marginVertical: 12 }} title="Register" />
+								<View style={{ flexDirection: "row" }}>
+									<Text weight="light" size={14} color={colors.textLight}>
+										Already have an account?
+									</Text>
+									<Text
+										weight="bold"
+										size={14}
+										color={colors.text}
+										style={{
+											marginHorizontal: 4,
+											borderBottomColor: colors.text,
+											borderBottomWidth: 1,
+										}}
+									>
+										Login
+									</Text>
+									<Text weight="light" size={14} color={colors.textLight}>
+										here!
+									</Text>
+								</View>
+							</View>
+						</View>
+						<View style={styles.separator}>
+							<View style={styles.line} />
+							<View>
+								<Text size={14} color={colors.textLight} style={{ paddingHorizontal: 8 }}>
+									Register With
+								</Text>
+							</View>
+							<View style={styles.line} />
+						</View>
+						<View style={styles.authWrapper}>
+							<Image
+								source={require("../../assets/images/google-circle.png")}
+								style={styles.socialMedia}
+							/>
+							<Image
+								source={require("../../assets/images/google-circle.png")}
+								style={styles.socialMedia}
+							/>
 						</View>
 					</View>
-				</View>
-				<View style={styles.separator}>
-					<View style={styles.line} />
-					<View>
-						<Text size={14} color={colors.textLight} style={{ paddingHorizontal: 8 }}>
-							Register With
-						</Text>
-					</View>
-					<View style={styles.line} />
-				</View>
-				<View style={styles.authWrapper}>
-					<Image
-						source={require("../../assets/images/google-circle.png")}
-						style={styles.socialMedia}
-					/>
-					<Image
-						source={require("../../assets/images/google-circle.png")}
-						style={styles.socialMedia}
-					/>
-				</View>
-			</ScrollView>
+				</TouchableWithoutFeedback>
+			</SafeAreaView>
 		</KeyboardAvoidingView>
 	);
 };
@@ -76,10 +88,9 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		flexDirection: "column",
-		justifyContent: "space-between",
+		justifyContent: "flex-end",
 		alignItems: "center",
 		backgroundColor: colors.background,
-		paddingBottom: "10%",
 	},
 	iconWrapper: {
 		flex: 1,
@@ -107,6 +118,7 @@ const styles = StyleSheet.create({
 	authWrapper: {
 		flex: 1,
 		flexDirection: "row",
+		paddingBottom: "10%",
 	},
 	socialMedia: {
 		width: 42,
